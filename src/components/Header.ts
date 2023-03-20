9a33e6f78e27125b03fd50f14fa29f0cd1d9c68f
yong <tkddbs1644@gmail.com>
2023-03-20 21:10:38 +0900

    feat(components/Header): Header 컴포넌트 레이아웃 및 스타일링

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -1,7 +1,20 @@
+import './Header.scss'
 import { Component } from '@/lib/core'
 
 export default class Header extends Component {
   template(): string {
-    return `HeaderComponent`
+    return `
+      <header>
+        <div class="titles">
+          <div>header</div>
+        </div>
+        <div class="actions">
+          <button>공유</button>
+          <button>업데이트</button>
+          <button>즐겨찾기</button>
+          <button>기타</button>
+        </div>
+      </header>
+    `
   }
 }
