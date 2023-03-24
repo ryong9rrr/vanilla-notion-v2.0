548ca0d46bc48139af9d4ece2ed7f342c6c84791
yong <tkddbs1644@gmail.com>
2023-03-25 00:43:16 +0900

    test(documentStore): helpers.test.ts 코드 리팩터링

--- src/document-store/helpers.test.ts
+++ src/document-store/helpers.test.ts
@@ -4,27 +4,32 @@ import { findPath } from './helpers'
 
 const DOCUMENTS = documentData.data as IDocument[]
 
-describe('documentStore test', () => {
-  test('findPath() 예외 처리 : 만약 존재하지 않는 id로 진입했다면 빈 배열을 반환한다.', () => {
-    expect(findPath(DOCUMENTS, 11111)).toEqual([])
-  })
-
-  test('findPath() 테스트 케이스 1', () => {
-    expect(findPath(DOCUMENTS, 33702)).toEqual([{ id: 33702, title: '상윤의 노션블로그' }])
-  })
-
-  test('findPath() 테스트 케이스 2', () => {
-    expect(findPath(DOCUMENTS, 43406)).toEqual([
+const findPath_TEST_CASE: [IDocument[], number, any][] = [
+  [DOCUMENTS, 11111, []], // 만약 존재하지 않는 id로 진입했다면 빈 배열을 반환한다.
+  [DOCUMENTS, 33702, [{ id: 33702, title: '상윤의 노션블로그' }]],
+  [
+    DOCUMENTS,
+    43406,
+    [
       { id: 33702, title: '상윤의 노션블로그' },
       { id: 43406, title: 'TILdd' },
-    ])
-  })
-
-  test('findPath() 테스트 케이스 3', () => {
-    expect(findPath(DOCUMENTS, 48239)).toEqual([
+    ],
+  ],
+  [
+    DOCUMENTS,
+    48239,
+    [
       { id: 48236, title: '리팩터링 주우우우웅' },
       { id: 48238, title: '안녕안녕' },
       { id: 48239, title: '안녕안ㄴ영연ㅇㅇ' },
-    ])
+    ],
+  ],
+]
+
+describe('documentStore test', () => {
+  test('findPath() : 문서의 경로를 배열로 반환한다.', () => {
+    findPath_TEST_CASE.forEach(([documents, documentId, expected]) => {
+      expect(findPath(documents, documentId)).toEqual(expected)
+    })
   })
 })
