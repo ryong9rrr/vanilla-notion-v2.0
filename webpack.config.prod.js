8a6c64bf2be6c359ddb1331d8eb24e5900640e03
yong <tkddbs1644@gmail.com>
2023-03-31 19:27:42 +0900

    prod: webpack.DefindPlugin을 제거, Dotenv에 옵션 설정

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,55 +1,46 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const Dotenv = require('dotenv-webpack')
-const dotenv = require('dotenv')
 const path = require('path')
 
-const webpack = require('webpack')
-
-module.exports = (env) => {
-  dotenv.config()
-
-  return {
-    mode: 'production',
-    entry: './src/index.ts',
-    module: {
-      rules: [
-        {
-          test: /\.ts$/,
-          exclude: /node_modules/,
-          loader: 'ts-loader',
-        },
-        {
-          test: /\.s?css$/,
-          use: [
-            'style-loader',
-            'css-loader',
-            {
-              loader: 'sass-loader',
-              options: {
-                additionalData: `
-                  @import "@/styles/_variables";
-                `,
-              },
-            },
-          ],
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['.ts', '.tsx', '.js', '.json'],
-      alias: {
-        '@': path.resolve(__dirname, 'src'),
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
       },
-    },
-    plugins: [
-      new Dotenv(),
-      new webpack.DefinePlugin({
-        API_END_POINT: JSON.stringify(process.env.API_END_POINT),
-        USER_NAME: JSON.stringify(process.env.USER_NAME),
-      }),
-      new HtmlWebpackPlugin({
-        template: 'index.html',
-      }),
+      {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              additionalData: `
+                @import "@/styles/_variables";
+              `,
+            },
+          },
+        ],
+      },
     ],
-  }
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [
+    new Dotenv({
+      systemvars: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
 }
